4ba9b5040fb8673d1f4fd2e91b29c93a2883b0da
Gabrial-8467 <gabrialdeora004github@gmail.com>
2023-08-12 09:38:07 +0530

    Initial commit

---
+++ Virtual_Mouse_by_GabrialDeora.py
@@ -0,0 +1,44 @@
+import cv2
+import mediapipe as mp
+import pyautogui
+cap = cv2.VideoCapture(0)
+hand_detector = mp.solutions.hands.Hands()
+drawing_utils = mp.solutions.drawing_utils
+screen_width, screen_height = pyautogui.size()
+index_y = 0
+while True:
+    _,frame = cap.read()
+    frame = cv2.flip(frame, 1)
+    frame_height, frame_width, _= frame.shape
+    rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    output = hand_detector.process(rgb_frame)
+    hands = output.multi_hand_landmarks
+    if hands:
+        for hand in hands:
+            drawing_utils.draw_landmarks(frame, hand)
+            landmarks = hand.landmark
+            for id, landmark in enumerate(landmarks):
+                x = int(landmark.x*frame_width)
+                y = int(landmark.y*frame_height)
+                if id == 8:
+                    cv2.circle(img=frame, center=(x,y), radius=15, color=(0, 255, 255))
+                    index_x = screen_width/frame_width*x
+                    index_y = screen_height/frame_height*y
+                    pyautogui.moveTo(index_x, index_y)
+                if id == 4:
+                       cv2.circle(img=frame, center=(x,y), radius=15, color=(0, 255, 255))
+                       thumb_x = screen_width/frame_width*x
+                       thumb_y = screen_height/frame_height*y
+                       print('outside', abs(index_y - thumb_y))
+                       if abs(index_y - thumb_y) < 50:
+                          pyautogui.click()
+                          pyautogui.sleep(0)
+                
+    cv2.imshow('virtual Mouse', frame)
+    # Key13==ENTER_KEY
+    if cv2.waitKey(1) == 13:
+        break
+# releasing_webcam
+cap.release()
+# destroying_window
+cv2.destroyAllWindows
